279d0281de4c506477e52ab27d1683fb73ce3870
unknown <Karanvir011998@gmail.com>
2025-01-11 18:38:46 +0530

    added gihub pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from './components/ui/toaster.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>
 )
