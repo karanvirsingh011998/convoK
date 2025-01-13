2c3255d16ce286718be45f2ef638621aeb6d6983
karanvirsingh011998 <110287106+karanvirsingh011998@users.noreply.github.com>
2025-01-13 12:21:16 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -123,7 +123,7 @@ export function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <Link href="/register" className="underline underline-offset-4">
+              <Link to="/register" className="underline underline-offset-4">
                 Sign up
               </Link>
             </div>
