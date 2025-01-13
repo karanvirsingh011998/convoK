131f4f1ad659d847af9688225421d03c57536696
karanvirsingh011998 <110287106+karanvirsingh011998@users.noreply.github.com>
2025-01-13 12:25:13 +0530

    Update PublicLayout.tsx

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,15 +1,15 @@
 import FooterComponent from "../components/footer"
 import { ThemeToggle } from "../components/theme-toggle"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 export const PublicLayout = () => {
   return (
     <div className="h-screen flex flex-col">
       <header className="border-b">
         <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-          <a href="/">
+          <Link to="/">
           <h1 className="text-xl font-bold">ConvoK</h1>
-          </a>
+          </Link>
           <ThemeToggle />
         </div>
       </header>
