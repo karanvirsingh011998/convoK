49c894c2165b77033fe7267759c8387b344cd128
karanvirsingh011998 <110287106+karanvirsingh011998@users.noreply.github.com>
2025-01-14 14:58:13 +0530

    Update auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,6 @@
 export const logout = () => {
   sessionStorage.removeItem('currentUser');
-   window.location.href = '/';
+   window.location.href = '/convoK';
 }; 
 
 export const initializeAdminUser = () => {
